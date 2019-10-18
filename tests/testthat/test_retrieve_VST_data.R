917faef82501fc9e4c97ac433b709e31c0c9f732
vscholl <victoria.scholl@colorado.edu>
2019-10-18 14:04:11 -0600

    updated the test checking if pulled data is a data frame

--- tests/testthat/test_retrieve_VST_data.R
+++ tests/testthat/test_retrieve_VST_data.R
@@ -1,5 +1,7 @@
 test_that("check if the package retrieves field data as data.frame", {
+  # retrieve data for the OSBS site for 2019
   tst <- retrieve_VST_data(site = "OSBS", start = 2019)
-  expect_type(tst, list)
+  # check that the data retrieved is a data frame
+  expect_is(tst,'list')
 
 })
