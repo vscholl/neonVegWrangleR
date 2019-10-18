1203672e69bef7c23d660bce0ec6fec96bd05e5e
vscholl <victoria.scholl@colorado.edu>
2019-10-18 14:25:50 -0600

    updating the tests so each function has its own test, and we also test multiple functions together

--- R/calc_tree_geolocations.R
+++ R/calc_tree_geolocations.R
@@ -6,22 +6,22 @@
 #' @seealso [geoNEON::def.calc.geo.os()] which this function wraps
 #' @export
 #' @examples
-#' calc_tree_geolocations(data = vst, dataProd = "vst_mappingandtagging")
+#' calc_tree_geolocations(data = vst)
 #'
 #'
 #'
-calc_tree_geolocations <- function(data = vst,
-                                   dataProd = "vst_mappingandtagging"){
+calc_tree_geolocations <- function(data = vst
+                                   , dataProd = "vst_mappingandtagging"){
 
   # using the NEON function, calculate precise geolocations for each
-  # mapped individual, from distance & azimuth to easting & northing
-  vst_loc <- geoNEON::def.calc.geo.os(data = veg_mapping_unique
-                              , dataProd = "vst_mappingandtagging") %>%
+  # mapped individual, from distance & azimuth to easting & northing.
+  # must specify "vst_mappingandtagging" as the data product.
+  vst_loc <- geoNEON::def.calc.geo.os(data = vst
+                              , dataProd = dataProd) %>%
 
     # Rename the adjEasting and adjNorthing columns
     dplyr::rename(easting = adjEasting
                   , northing = adjNorthing)
-
 
   return(vst_loc)
 }
