ae8af6ffe77ba02a5085e568fdc2730d8f0348ca
vscholl <victoria.scholl@colorado.edu>
2019-10-18 14:01:19 -0600

    modifying the tests for retrieving VST data and we need to update the coordinates test using the geoNEON geolocation step

--- tests/testthat/test_retrieve_VST_data.R
+++ tests/testthat/test_retrieve_VST_data.R
@@ -1,5 +1,5 @@
 test_that("check if the package retrieves field data as data.frame", {
   tst <- retrieve_VST_data(site = "OSBS", start = 2019)
-  expect_type(tst, data.frame)
+  expect_type(tst, list)
 
 })
